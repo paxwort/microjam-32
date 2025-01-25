eb54283f847806d948220be9e256b0266f218fee
paxwort <65900711+paxwort@users.noreply.github.com>
2025-01-25 03:35:38 +0000

    Really awful path generation that technically works

--- scripts/Maze.gd
+++ scripts/Maze.gd
@@ -1,4 +1,4 @@
-class_name Maze extends Node
+class_name Maze extends Node3D
 
 var cells : Array[Array]
 var size_x : int = 20
@@ -6,9 +6,106 @@ var size_y : int = 20
 signal created
 
 func _ready():
+	flood_empty()
+	var start = Vector2i(0, size_y / 2)
+	generate_maze(start)
+	var path : Array[Vector2i]= []
+	path = find_nice_path(start)
+	$MazePath.curve_changed.connect(print.bind("change"))
+	flood_empty()
+	cut_path(path)
+	set_game_path(path)
+	created.emit()
+	
+
+func get_cell(x, y) -> int:
+	if x >= 0 and y >= 0 and x < size_x and y < size_y:
+		return cells[x][y]
+	return 0
+	
+func set_cell(x, y, val) -> void:
+	if x >= 0 and y >= 0 and x < size_x and y < size_y:
+		cells[x][y] = val
+
+func set_game_path(path : Array[Vector2i]):
+	var curve = Curve3D.new()
+	for v in path:
+		curve.add_point(Vector3(v.x, 0, v.y))
+	$MazePath.curve = curve
+
+func cut_path(path : Array[Vector2i]):
+	for v in path:
+		set_cell(v.x, v.y, 1)
+
+func flood_empty() -> void:
 	cells = []
 	for x in range(0,size_x):
 		cells.push_back([])
 		for y in range(0, size_y):
-			cells[x].push_back(randi_range(0,1))
-	created.emit()
+			cells[x].push_back(0)
+
+func generate_maze(start : Vector2i):
+	var frontier : Array
+	frontier.push_back([start.x, start.y, start.x, start.y])
+	while frontier.size() > 0:
+		var random_index = randi_range(0, frontier.size() - 1)
+		var f = frontier.pop_at(random_index)
+		var u = f[0]
+		var v = f[1]
+		var x = f[2]
+		var y = f[3]
+		if(cells[x][y] == 0):
+			cells[u][v] = 1
+			cells[x][y] = 1
+			if (x >= 2) and (cells[x - 2][y] == 0):
+				frontier.push_back([x - 1, y, x - 2, y])
+			if (y >= 2) and (cells[x][y - 2] == 0):
+				frontier.push_back([x, y - 1, x, y - 2])
+			if (x < size_x - 2) and (cells[x + 2][y] == 0):
+				frontier.push_back([x + 1, y, x + 2, y])
+			if (y < size_y - 2) and (cells[x][y + 2] == 0):
+				frontier.push_back([x, y + 1, x, y + 2])
+
+class DijkstraNode:
+	var position : Vector2i
+	var distance : int
+	var prev : DijkstraNode
+	func _init(p_position : Vector2i, p_distance : int, p_prev : DijkstraNode = null):
+		position = p_position
+		distance = p_distance
+		prev = p_prev
+
+#Dijkstra the maze to find a nice path by some heuristic
+func find_nice_path(start : Vector2i):
+	var open_set = [DijkstraNode.new(start, 0)]
+	var visited = {}
+	var distances = {}
+	var neighbour_offsets = [Vector2i(-1,0),Vector2i(1,0),Vector2i(0,1),Vector2i(0,-1)]
+	var best_node : DijkstraNode
+	best_node = open_set[0]
+	while open_set.size() > 0:
+		open_set.sort_custom(func(a,b) : return a.distance < b.distance)
+		var current = open_set.pop_front()
+		if(visited.has(current.position)):
+			continue
+		visited[current.position] = true
+		
+		if(current.distance > best_node.distance):
+			var threshhold = 4
+			if(Vector2i(size_x / 2, size_y / 2) - current.position).length() < threshhold:
+				best_node = current
+		
+		for offset in neighbour_offsets:
+			var next_position = current.position + offset
+			if get_cell(next_position.x, next_position.y) == 1:
+				var new_distance = current.distance + 21
+				if not distances.has(next_position) or new_distance < distances[next_position].distance:
+					var next_node = DijkstraNode.new(next_position, new_distance, current)
+					distances[next_position] = next_node
+					open_set.push_back(next_node)
+	
+	var path : Array[Vector2i]= []
+	while (best_node != null):
+		path.push_front(best_node.position)
+		best_node = best_node.prev
+	return path
--- scripts/MazeMesher.gd
+++ scripts/MazeMesher.gd
@@ -1,20 +1,17 @@
 class_name MazeMesher extends GridMap
 
-@export var maze : Maze
+@onready var maze : Maze = get_parent()
 	
 func _on_maze_created():
 	clear()
-	set_cells_from_maze(maze.size_x, maze.size_y)
+	set_cells_from_maze(0, maze.size_x - 1, 0, maze.size_y - 1)
 
-func set_cells_from_maze(size_x : int, size_y : int):
-	for x in range(0,size_x):
-		for y in range(0,size_y):
+func set_cells_from_maze(min_x : int, max_x : int, min_y : int, max_y : int):
+	for x in range(min_x,max_x):
+		for y in range(min_y,max_y):
 			var square_of_cells : int = 0
-			square_of_cells |= (0b1 if maze.cells[x][y] == 1 else 0)
-			if y < size_y - 1:
-				square_of_cells |= (0b10 if maze.cells[x][y+1] == 1 else 0)
-			if x < size_x - 1:
-				square_of_cells |= (0b100 if maze.cells[x+1][y] == 1 else 0)
-			if x < size_x - 1 and y < size_y - 1:
-				square_of_cells |= (0b1000 if maze.cells[x+1][y+1] == 1 else 0)
+			square_of_cells |= (0b1 if maze.get_cell(x,y) == 1 else 0)
+			square_of_cells |= (0b10 if maze.get_cell(x,y+1) == 1 else 0)
+			square_of_cells |= (0b100 if maze.get_cell(x+1,y) == 1 else 0)
+			square_of_cells |= (0b1000 if maze.get_cell(x+1,y+1) == 1 else 0)
 			set_cell_item(Vector3i(x,0,y), square_of_cells)
