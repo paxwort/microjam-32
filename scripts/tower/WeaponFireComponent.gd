ee452dd915f49578cacff5b98bfb98667a5bfe50
QG <QGeeDev@users.noreply.github.com>
2025-01-26 14:59:25 +0000

    Towers can now fire weapons

--- scripts/tower/WeaponFireComponent.gd
+++ scripts/tower/WeaponFireComponent.gd
@@ -16,17 +16,17 @@ func _process(delta: float) -> void:
 	if cooldown > 0:
 		cooldown-=delta
 
-func fire_weapon(target_pos: Vector3) -> void:
+func fire_weapon(target_pos: Transform3D) -> void:
 	if(cooldown <= 0):
 		cooldown = 1.0/fire_rate
 		
 		var new_projectile = projectile.instantiate()
-		new_projectile.global_position = projectile_spawn_point.global_position
-		new_projectile.look_at(target_pos, Vector3(0,1,0))
-		new_projectile.rotation.x = deg_to_rad(90)
-		new_projectile.scale = Vector3(0.05,0.05,0.05)
-		
-		
-		
 		get_tree().root.add_child(new_projectile)
+		new_projectile.global_position = projectile_spawn_point.global_position
+		new_projectile.scale = Vector3(0.02,0.02,0.02)
+		
+		new_projectile.look_at(target_pos.origin, Vector3.UP)
+		new_projectile.rotation.z = deg_to_rad(90)
+		new_projectile.set_speed(projectile_speed)
+		print(new_projectile.rotation)
 		
