aeb953fd06e1f58af9a8bffb4fc13e277300bd26
paxwort <65900711+paxwort@users.noreply.github.com>
2025-01-26 18:29:09 +0000

    towers have animations, spikes do damage

--- scripts/tower/LookAtTarget.gd
+++ scripts/tower/LookAtTarget.gd
@@ -6,4 +6,4 @@ func _physics_process(delta: float) -> void:
 	if (tracker != null) and (tracker.targeted != null):
 		var target_position = tracker.targeted.global_position
 		var diff = global_position - target_position
-		rotation.y = atan2(diff.x, diff.z)
+		rotation.y = lerp(rotation.y, atan2(diff.x, diff.z), delta * 10)
